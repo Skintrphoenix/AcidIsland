e45e4d603f9162f0e42851cbd0fee6d119e24318
Skintrphoenix <72775104+Skintrphoenix@users.noreply.github.com>
2021-04-01 14:39:35 +0700

    Update AcidIsland.php

--- Skintrphoenix/AcidIsland/AcidIsland.php
+++ Skintrphoenix/AcidIsland/AcidIsland.php
@@ -11,11 +11,31 @@ use pocketmine\command\CommandSender;
 use pocketmine\block\BlockFactory;
 use pocketmine\Player;
 
+#commands
+use Skintrphoenix\AcidIsland\Command\Home;
+use Skintrphoenix\AcidIsland\Command\Create;
+use Skintrphoenix\AcidIsland\Command\Team;
+use Skintrphoenix\AcidIsland\Command\Settings;
+use Skintrphoenix\AcidIsland\Command\Warp;
+use Skintrphoenix\AcidIsland\Command\Setspawn;
+use Skintrphoenix\AcidIsland\Command\Reset;
+
 class AcidIsland extends PluginBase implements Listener
 {
 	private static $instance;
 	
-	public $commands = [];
+	private $home;
+	private $create;
+	private $team;
+	private $settings;
+	private $warp;
+	private $setspawn;
+	private $reset;
+	
+	
+	public static function getInstance(): AcidIsland {
+        return self::$instance;
+    }
 	
 	public function onEnable(){
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
@@ -30,7 +50,7 @@ class AcidIsland extends PluginBase implements Listener
             case "island":
                 if($sender instanceof Player){
                     if(!isset($args[0])){
-                    	$sender->sendMessage(" - /island home \n - /island create \n - /island team \n -/island settings \n - /island warp");
+                    	$sender->sendMessage(" - /island home \n - /island create \n - /island team \n -/island settings \n - /island warp \n - /island setspawn \n - /island reset");
                         return true;
                     }
                     $arg = array_shift($args);
@@ -45,15 +65,18 @@ class AcidIsland extends PluginBase implements Listener
                         break;
                         case "warp":
                         break;
+                        case "setspawn":
+                        break;
+                        case "reset":
+                        break;
+                        case "help":
+                        $sender->sendMessage(" - /island home \n - /island create \n - /island team \n -/island settings \n - /island warp \n - /island setspawn \n - /island reset");
+                        break;
                     }
                 }
             break;
         }
         return true;
-    }
-    
-    public static function getInstance(): self {
-        return self::$instance;
     }
     
   
